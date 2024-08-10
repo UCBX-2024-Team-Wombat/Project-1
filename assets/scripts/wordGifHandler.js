dfe3c7556bb7b62a0e1ac18638317f02cf432261
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-08-10 14:34:33 -0700

    Apply formatter, add reset function

--- assets/scripts/wordGifHandler.js
+++ assets/scripts/wordGifHandler.js
@@ -1,30 +1,33 @@
 // Variables
-const localStorageKeyName = 'project-1-giphy-api-key';
-const giphyBaseURL = 'https://api.giphy.com/v1/gifs/search?';
+const localStorageKeyName = "project-1-giphy-api-key";
+const giphyBaseURL = "https://api.giphy.com/v1/gifs/search?";
+const embedSlot = document.getElementById("gif-embed-slot");
 
 // Functions
-function fetchWordGif(word){
+function fetchWordGif(word) {
   // Create callout string
-  const queryString = giphyBaseURL + new URLSearchParams({
-    api_key: getGiphyApiKey(),
-    q: word,
-    limit: 25,
-    rating: 'pg-13',
-    lang: 'en'
-  }).toString();
+  const queryString =
+    giphyBaseURL +
+    new URLSearchParams({
+      api_key: getGiphyApiKey(),
+      q: word,
+      limit: 25,
+      rating: "pg-13",
+      lang: "en",
+    }).toString();
 
   // Make callout
   fetch(queryString)
-    .then( response => {
+    .then((response) => {
       // Format infoStream to Json
       return response.json();
     })
-    .then( function(data) {
+    .then(function (data) {
       // Create list for returned gif links
-      const availableGifEmbedLinks = []
+      const availableGifEmbedLinks = [];
 
       // Populate list with embed urls from returned data
-      for(const gif of data.data){
+      for (const gif of data.data) {
         availableGifEmbedLinks.push(gif.embed_url);
       }
 
@@ -33,25 +36,25 @@ function fetchWordGif(word){
 
       // Render selected gif embed link
       renderGif(availableGifEmbedLinks[randomIndex]);
-    })
+    });
 }
 
-function renderGif(gifEmbedLink){
+function renderGif(gifEmbedLink) {
   // Clear default text of embedSlot (provided by index.js)
-  embedSlot.innerText = '';
+  embedSlot.innerText = "";
 
   // Create new embed element and populate with link
-  const embedElement = document.createElement('embed');
+  const embedElement = document.createElement("embed");
   embedElement.src = gifEmbedLink;
-  embedElement.height = '100%';
-  embedElement.width = '100%';
+  embedElement.height = "100%";
+  embedElement.width = "100%";
 
   // Append to render
   embedSlot.appendChild(embedElement);
 }
 
-// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
-function getRandomInteger(items){
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInteger(items) {
   return Math.floor(Math.random() * items);
 }
 
@@ -62,4 +65,9 @@ function storeGiphyApiKey(apiKey) {
 
 function getGiphyApiKey() {
   return localStorage.getItem(localStorageKeyName);
+}
+
+function resetGif() {
+  // Reset gif
+  embedSlot.innerHTML = null;
 }
