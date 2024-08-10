daa5fdbb5a2a79c5e642adcdb07bbeea8531d359
Yasemin Vatan <yaseminvatan@Yasemins-MacBook-Pro.local>
2024-08-10 05:18:06 -0700

    modalHanler.js modified. showRelatedWords function from relatedWordsHandler.js to populate the modal when the button is clicked.click event added onchild elements

---
+++ assets/scripts/modalHandler.js
@@ -0,0 +1,42 @@
+import { showRelatedWords } from './relatedWordsHandler.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // this part is to open and close modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            const word = document.querySelector('input[type="text"]').value;
+            showRelatedWords(word); 
+            openModal($target);
+        });
+    });
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === "Escape") {
+            closeAllModals();
+        }
+    });
+});
