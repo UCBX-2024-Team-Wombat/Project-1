ca72d568040204749fe3b6d6e9a50bca0053bfc6
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-08-10 15:38:57 -0700

    Feature input validation (#34)
    * Add word validation
    
    * Correct class assigned to modal display

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,20 +1,44 @@
 // Variables
 const searchInput = document.getElementById("searched-word");
 const searchButton = document.getElementById("search");
+const invalidWordNotice = document.getElementById('invalid-word-notice');
 
 // Execution
 searchButton.addEventListener("click", handleSearch);
 
 // Functions
 function handleSearch(event) {
-  resetPage();
-  const searchedWord = searchInput.value;
-  fetchRelatedWords(searchedWord);
-  fetchWordGif(searchedWord);
+  const searchedWord = searchInput.value.trim();
+  if(isValidWord(searchedWord)){
+    hideInvalidWordNotice();
+    resetPage();
+    fetchRelatedWords(searchedWord);
+    fetchWordGif(searchedWord);
+  }
+  else {
+    displayInvalidWordNotice();
+  }
 }
 
 // Reset page elements between new word searches
 function resetPage() {
   resetModal();
   resetGif();
+}
+
+function isValidWord(word){
+  
+  // Regex pattern derived from modification of pattern
+  // provided here: https://regex101.com/r/caXjPb/1
+  const pattern = /^(?:[a-z-']{2,}|-?)$/;
+
+  return pattern.test(word);
+}
+
+function displayInvalidWordNotice(){
+  invalidWordNotice.removeAttribute('hidden');
+}
+
+function hideInvalidWordNotice(){
+  invalidWordNotice.setAttribute('hidden', 'true');
 }
